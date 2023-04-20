5aa41bd33afad65f6028991dce1c2b33b0d6d459
Anton <antofffka@inbox.ru>
2023-04-20 15:32:13 +0300

    Initial commit

---
+++ src/main/java/Model/DefaultController.java
@@ -0,0 +1,12 @@
+package Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
